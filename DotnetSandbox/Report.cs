38c9f00b48d4f7f8b41717ff8ade5ad9bf8e61ce
AlterraDeveloper <jkiselev2411@gmail.com>
2022-03-28 19:18:55 +0600

    add fake report

---
+++ DotnetSandbox/Report.cs
@@ -0,0 +1,28 @@
+﻿namespace DotnetSandbox
+{
+    public class Report
+    {
+        public Report()
+        {
+            
+        }
+
+        public void Build()
+        {
+            var column1 = new ReportColumn("1");
+            var column2 = new ReportColumn("2");
+            var column3 = new ReportColumn("3");
+            var column4 = new ReportColumn("4");
+            var column5 = new ReportColumn("5");
+            var column6 = new ReportColumn("6");
+        }
+    }
+
+    public class ReportColumn
+    {
+        public ReportColumn(string columnName)
+        {
+            
+        }
+    }
+}
