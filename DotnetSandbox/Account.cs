e92789479585210674345fb4f33401efbfc4998b
AlterraDeveloper <jkiselev2411@gmail.com>
2022-04-01 19:07:52 +0600

    add Account model

---
+++ DotnetSandbox/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetSandbox
+{
+    public class Account
+    {
+        public int CustomerID { get; set; }
+
+        public string AccountNo { get; set; }
+
+        public int CurrencyID { get; set; }
+    }
+}
