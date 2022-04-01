5b30fdeed894bcabd1e855ea749f1a640e5a4ce6
AlterraDeveloper <jkiselev2411@gmail.com>
2022-04-01 19:40:18 +0600

    complete task#1

---
+++ DotnetSandbox/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace DotnetSandbox
+{
+    /// <summary>
+    /// Модель счета
+    /// </summary>
+    public class Account
+    {
+        public int CustomerID { get; set; }
+
+        public string AccountNo { get; set; }
+
+        public int CurrencyID { get; set; }
+    }
+}
